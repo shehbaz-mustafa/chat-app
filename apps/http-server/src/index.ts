c613e4eae5ae6146bf2311b1509ecd6f084d0f71
Shaan2170 <shaanmustafa1234@gmail.com>
2026-06-11 16:47:23 +0530

    feat: add TypeScript configuration and new UI components

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,18 @@
+
+import express from "express";
+
+const app = express();
+
+app.get("/signup", (req, res) => {
+    res.send("Hello World");
+})
+
+app.get("/signin", (req, res) => {
+    res.send("Hello World");
+})
+
+app.get("/chat", (req, res) => {
+    res.send("Hello World");
+})
+
+app.listen(3000);
---
+++ packages/ui/src/text-input.tsx
@@ -0,0 +1,20 @@
+
+interface ProtoType {
+    placeholder: string;
+    size: "big" | "small",
+    onChange: (e: any) => void;
+}   
+
+
+export function TextInput ({
+    placeholder,
+    size,
+    onChange 
+}: ProtoType) {
+    return <input onChange={onChange} placeholder={placeholder} style={{
+        padding: size === "big" ? 20 : 10,
+        margin: size === "big" ? 20 : 10,
+        borderColor: "black",
+        borderWidth: 1,
+    }}></input>
+}
